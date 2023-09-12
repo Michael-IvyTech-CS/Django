3c470fe5031adfff981e618acb5bf2865ebdaa74
Michael-IvyTech-CS <mbarthauer@ivytech.edu>
2023-09-12 18:04:36 -0400

    added django files

---
+++ djangogirls/myvenv/Scripts/django-admin.py
@@ -0,0 +1,21 @@
+#!C:\Users\bmich\Desktop\Courses\Ivy_Tech_SDEV220_2\Code\M04\Django\djangogirls\myvenv\Scripts\python.exe
+# When the django-admin.py deprecation ends, remove this script.
+import warnings
+
+from django.core import management
+
+try:
+    from django.utils.deprecation import RemovedInDjango40Warning
+except ImportError:
+    raise ImportError(
+        'django-admin.py was deprecated in Django 3.1 and removed in Django '
+        '4.0. Please manually remove this script from your virtual environment '
+        'and use django-admin instead.'
+    )
+
+if __name__ == "__main__":
+    warnings.warn(
+        'django-admin.py is deprecated in favor of django-admin.',
+        RemovedInDjango40Warning,
+    )
+    management.execute_from_command_line()
