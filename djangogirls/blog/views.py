944c74eb2a27c8145cc5e70a2c0c482c07aac9d4
Michael-IvyTech-CS <mbarthauer@ivytech.edu>
2023-10-04 16:18:53 -0400

    Added post_new to views.py

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -13,3 +14,9 @@ def post_detail(request, pk):
     """Defines the view 'post_detail' which displays a single blog post in detail."""
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    """Defines the view 'post_new' which displays a form for creating a new blog post."""
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
