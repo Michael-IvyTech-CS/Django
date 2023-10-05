d95dfffbc98e97869a9fd0ac3164f5141f8f0c42
Michael-IvyTech-CS <mbarthauer@ivytech.edu>
2023-10-05 12:23:59 -0400

    Added edit functionality

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -29,3 +29,19 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    """Defines the view 'post_edit' which displays a form for editing a previous blog post."""
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
